b6d1cb4ca6d6a3b8215708631f51e5557f662d84
Marcin Grabowski <mgrabowski87@gmail.com>
2019-09-19 13:39:12 +0200

    Add visual products on the page

--- src/js/script.js
+++ src/js/script.js
@@ -51,17 +51,53 @@
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
+  class Product {
+    constructor(id, data) {
+      this.id = id;
+      this.data = data;
+      const thisProduct = this;
+      thisProduct.renderInMenu();
+      /* console.log('new Product:', thisProduct); */
+    }
+    renderInMenu() {
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+      /* console.log(menuContainer); */
+    }
+  }
+
+
 
   const app = {
-    init: function(){
+
+    initMenu: function () {
+      const thisApp = this;
+      console.log('thisApp.data', thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+
+      }
+    },
+    initData: function () {
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
+
     },
   };
 
   app.init();
+
 }
